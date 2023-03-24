eacfc3dd9a7b7f5bbb2ec065a7cbe2d00ad4bcfc
unknown <aminehlab@gmail.com>
2023-03-24 21:21:30 +0100

    adding stats

--- src/Containers/Verification/Verification.jsx
+++ src/Containers/Verification/Verification.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Verification.scss';
 
+import emailPic from '../../Assets/icons/email.png'
+
 const Verification = () => {
 	return (
-		<div>
-			Verification
-		</div>
+		<section className="ver_container app-flex">
+			<img src={emailPic} alt="emailing" className="email-pic"/>
+			<div>
+				<h4>This is a text example  and sdgas asdfnsdf saf d fs f sf sdf sad fsadfsadfsadf sdf sdf sdfasdf default</h4>
+				<h4>Please hold tight while we are verifying you email.</h4>
+				<h2>Loader</h2>
+			</div>
+			<span className="bar"></span>
+			<div>
+				<h5>Rights sections.</h5>
+			</div>
+		</section>
 	)
 }
 
