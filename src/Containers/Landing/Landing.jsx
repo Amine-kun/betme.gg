fc725fe8fda67bb1a70e50edea9d145f0a75703b
unknown <aminehlab@gmail.com>
2023-07-09 21:28:34 +0100

    updating judge download link

--- src/Containers/Landing/Landing.jsx
+++ src/Containers/Landing/Landing.jsx
@@ -112,7 +112,7 @@ const Landing = () => {
 				</Link>
 				<div className="app-flex">
 						{/*<button className="sub-btn download-btn " style={{marginTop:'15px'}}>Download Desktop App</button>*/}
-						<a className="sub-btn download-btn " style={{marginTop:'15px', padding:'12px 15px'}} href="https://download1337.mediafire.com/myajbaxhfvogjiAesShgRVx-_b1qxEZjjDOl6x7gnXAZnTV6EmjneviKf4e9zhfbhE1uD5xzo_mm51DCgfy3LZVEVSFhZmBGmYKVPSHVktISZ7Cuk4-Yj2wSpHGWBom821uWKuWc5Q3FwqGvhjbMfyP4z0JYI1tdZg29z2qK_g/p43euxrn0bjk694/SquidJudge+Setup1.0.0.exe" download>Download Squid</a>
+						<a className="sub-btn download-btn " style={{marginTop:'15px', padding:'12px 15px'}} href="https://download856.mediafire.com/ldz4qflyge0gCX4dzUKZH2s5wd_NkjQ9PHZg3GUFFcbjFfd5Ru3D2ggYFj69l67hO3p7aBHZCaXb1dQhEeii5BnMv5xnRhq1FIETGrGSjK706PudiLwedUH1_11-n--QsVUlSbnNyDdj4Yom95uqG0GhXohGFx8-8BL4Fj5RAg/0zae16hl8m7gnvg/SquidJudge+Setup+1.0.0.exe" download>Download Squid</a>
 				</div>
 			</div>
 
